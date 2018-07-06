90a93538e5c2ecc2e01250a925e58c9e59c78259
““EvilAngel0605git config --global user.email1125018106@qq.comgit commit -m unity3dgit config --global user.name ““EvilAngel0605 <1125018106@qq.com>
2018-07-06 11:14:38 +0800

    简单工厂模式-超市收银系统

---
+++ 设计模式/简单工厂模式/超市收银系统/CashNormal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 简单工厂模式.超市收银系统
+{
+    class CahNormal : CashSuper
+    {
+        public override double AcceptCash(double money)
+        {
+            return money;
+        }
+    }
+}
---
+++ 设计模式/简单工厂模式/超市收银系统/CashRebate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 简单工厂模式.超市收银系统
+{
+    class CashRebate : CashSuper
+    {
+        private double mMoneyRebate = 1;
+        public  CashRebate(string moneyRebate)
+        {
+            mMoneyRebate = double.Parse(moneyRebate);
+        }
+        public override double AcceptCash(double money)
+        {
+            return money * mMoneyRebate;
+        }
+    }
+}
